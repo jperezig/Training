423527206575fb361f12bb7501adbe40595f5597
Joaquin Perez-Iglesias <joaquin.perez.iglesias@gmail.com>
2014-04-05 09:30:51 +0100

    Initial Commit

---
+++ src/main/java/org/ninit/App.java
@@ -0,0 +1,13 @@
+package org.ninit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
